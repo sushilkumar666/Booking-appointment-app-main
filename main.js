42e82c5e3ae60eafa7048ceea63d9a5792d2b895
sushilkumar sharma <sushilssharma064@gmail.com>
2023-10-10 12:03:00 +0530

    assignment13 deleting from localstrage

--- main.js
+++ main.js
@@ -4,15 +4,37 @@ const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 const phoneInput = document.querySelector('#phone');
+const itemList = document.querySelector('#users');
 console.log(phoneInput);
 
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
+itemList.addEventListener('click', onDelete)
+
+
+function onDelete(e) {
+  var li = e.target.parentElement;
+  for (let i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i); // Get the key at index i
+    var firstChildText = li.firstChild.textContent; // Get the text content of the first child node
+    console.log(firstChildText);
+
+    // Check if the key is present in the text content
+    if (firstChildText.indexOf(key) !== -1) {
+      localStorage.removeItem(key); // Remove the item from localStorage
+    }
+  }
+
+  if (e.target.classList.contains('delete')) {
+    itemList.removeChild(li); // Remove the <li> element from the DOM
+  }
+}
+
 
 function onSubmit(e) {
   e.preventDefault();
-  
-  if(nameInput.value === '' || emailInput.value === '' || phoneInput == '') {
+
+  if (nameInput.value === '' || emailInput.value === '' || phoneInput.value === '') {
     // alert('Please enter all fields');
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
@@ -23,9 +45,15 @@ function onSubmit(e) {
     // Create new list item with user
     const li = document.createElement('li');
 
+
+    // creating delete button 
+    const deleteButton = document.createElement('button');
+    deleteButton.className = "btn btn-danger btn-sm float-end delete"
+    deleteButton.appendChild(document.createTextNode('Delete'))
+
     // Add text node with input values
     li.appendChild(document.createTextNode(`${nameInput.value} - ${emailInput.value} - ${phoneInput.value}`));
-
+    li.appendChild(deleteButton);
     // Add HTML
     // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
 
@@ -35,7 +63,7 @@ function onSubmit(e) {
 
 
     // storing uniquely every user details
-   
+
 
 
     // Append to ul
@@ -45,6 +73,7 @@ function onSubmit(e) {
     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
+    phoneInput.value = "";
 
 
   }
@@ -52,11 +81,11 @@ function onSubmit(e) {
     // Generate a unique key for the user (e.g., using a timestamp)
     const userKey = emailInput;
     const newUser = {
-        userName :  nameInput,
-        email    :  emailInput,
-        phone    : phoneInput
+      userName: nameInput,
+      email: emailInput,
+      phone: phoneInput
     }
-    
+
     // Store the user data in localStorage using the generated key
     localStorage.setItem(userKey, JSON.stringify(newUser));
   }
